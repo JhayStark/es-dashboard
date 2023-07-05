427953017f0e79e0d38c37dac0404a6051505304
JhayStark <jaamen5@gmail.com>
2023-07-05 14:52:08 +0000

    reports

--- components/DataTableBase.jsx
+++ components/DataTableBase.jsx
@@ -1,22 +1,22 @@
-import React from "react";
-import DataTable from "react-data-table-component";
-const selectProps = { indeterminate: (isIndeterminate) => isIndeterminate };
+import React, { useState } from 'react';
+import DataTable from 'react-data-table-component';
+const selectProps = { indeterminate: isIndeterminate => isIndeterminate };
 
 const rowStyles = [
   {
-    when: (row) => !row.id % 2 === 0,
+    when: row => !row.id % 2 === 0,
     style: {
-      backgroundColor: "white",
-      color: "black",
-      borderColor: "white",
+      backgroundColor: 'white',
+      color: 'black',
+      borderColor: 'white',
     },
   },
   {
-    when: (row) => row.id % 2 === 0,
+    when: row => row.id % 2 === 0,
     style: {
-      backgroundColor: "#EDF3FF",
-      color: "black",
-      borderColor: "#EDF3FF",
+      backgroundColor: '#EDF3FF',
+      color: 'black',
+      borderColor: '#EDF3FF',
     },
   },
 ];
@@ -24,26 +24,34 @@ const rowStyles = [
 const customStyles = {
   rows: {
     style: {
-      paddingTop: "1.5rem",
-      paddingBottom: "1.5rem",
-      fontSize: "1.05rem",
+      paddingTop: '1.5rem',
+      paddingBottom: '1.5rem',
+      fontSize: '1.05rem',
     },
   },
   headCells: {
     style: {
-      paddingTop: "1.5rem",
-      paddingBottom: "1.5rem",
-      fontSize: "1.05rem",
+      paddingTop: '1.5rem',
+      paddingBottom: '1.5rem',
+      fontSize: '1.05rem',
     },
   },
 };
 function DataTableBase(props) {
+  const [tableData, setTableData] = useState(props.data);
+  const [filterText, setFilterText] = useState('');
+  const filteredItems = tableData.filter(
+    item =>
+      item.name && item.name.toLowerCase().includes(filterText.toLowerCase())
+  );
+
   return (
     <DataTable
       pagination
       selectableRowsComponentProps={selectProps}
       dense
-      {...props}
+      columns={props.columns}
+      data={props.data}
       conditionalRowStyles={rowStyles}
       customStyles={customStyles}
       responsive
